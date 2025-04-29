8a60bfd7ad9938f173c4142a49bb293f6a65d736
reid ketchum <reidketchum@gmail.com>
2025-04-29 01:15:53 +0000

    Update ecosystem.config.js to use absolute path for script

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,8 +4,8 @@ module.exports = {
   apps : [
     {
       name        : 'LocalBarcodeVerifier', // Name for the local TUI app in PM2
-      script      : 'local-app.js',
-      cwd         : '.', // Current working directory
+      script      : '/home/pi/BarcodeVerifier/local-app.js', // Use absolute path
+      // cwd         : '.', // cwd might not be needed with absolute path, but can keep
       interpreter : 'node',
       // Ensure correct user/permissions for GPIO if needed.
       // You might need to configure PM2 user or run PM2 with sudo initially.
